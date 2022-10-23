3a92e7b8c2abb8b9d2a0ba58b2bad943f020b166
ShAkEeB <shakeeb@ShAkEeBs-MacBook-Pro.local>
2022-10-24 00:16:39 +0600

    add login and register structure

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -5,8 +5,36 @@ const Login = () => {
     const { } = useContext(AuthUser)
 
     return (
-        <div>
-            login
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col ">
+                <div className="text-center">
+                    <h1 className="text-5xl font-bold mb-2 text-green-400">Login now !</h1>
+                    <p>lets go to the news feed ,you must Login to see all element</p>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <label className="label">
+                                <p className="link link-hover"><small>Forgot password?</small></p>
+                            </label>
+                            <small>Dont have Accout go <span className='link link-hover text-teal-400'>Registation</span> Page</small>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -2,8 +2,46 @@ import React from 'react';
 
 const Register = () => {
     return (
-        <div>
-           regidtr 
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col ">
+                <div className="text-center">
+                    <h1 className="text-5xl font-bold mb-2 text-green-400">Register now !</h1>
+                    <p>Give All the nessesarry information for registet & ready to go</p>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Usrname</span>
+                            </label>
+                            <input type="text" placeholder="username" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo Url</span>
+                            </label>
+                            <input type="text" placeholder="url" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control  mb-8">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password" className="input input-bordered" />
+
+                        </div>
+                        <small>Alreay have account go <span className='link link-hover text-teal-400'>Login</span>Page</small>
+                        <div className="form-control mt-2">
+                            <button className="btn btn-primary">Register</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
